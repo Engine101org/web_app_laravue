13afc7f92c001cff22a37e1401c2c72aa62e4821
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-09-14 20:50:03 +0200

    fixed a bug :)

--- config/database.php
+++ config/database.php
@@ -1,11 +1,5 @@
 <?php
 
-$url = parse_url(getenv("mysql://b8f5252a2b365d:2166959e@us-cdbr-iron-east-01.cleardb.net/heroku_be5397c9555e4f3?reconnect=true"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 return [
 
     /*
@@ -46,11 +40,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
