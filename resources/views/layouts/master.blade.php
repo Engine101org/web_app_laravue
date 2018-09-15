ca7ac82a1e86d4ab87892cc7a259e879115c5306
kaysiz <ksiziva@gmail.com>
2018-09-15 14:10:23 +0200

    cleaning dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,12 @@
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      </li>
+    </ul>
     <!-- SEARCH FORM -->
     <form class="form-inline ml-3">
       <div class="input-group input-group-sm">
@@ -32,7 +38,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="./img/linux.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="./img/linux.png" alt="Engine 101" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Engine 101</span>
     </a>
