94faa865f9b862f5dd820b7962e445ea5b7a81b8
kaysiz <ksiziva@gmail.com>
2018-09-15 14:21:23 +0200

    further cleaning dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,7 +51,7 @@
           <img src="./img/man.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
