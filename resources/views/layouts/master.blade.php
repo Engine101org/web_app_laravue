f9314a300ab31518c77abf62aaf8e78ae21e6a76
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-09-22 06:13:06 +0200

    menu changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -80,17 +80,7 @@
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <p>Admin Users</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <p>Consultants</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <p>Schools</p>
+                  <p>      Users</p>
                 </a>
               </li>
             </ul>
